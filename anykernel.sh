38f001993d271c6ca19e695b87d6b023d3be2385
fukiame <fukiame@proton.me>
2024-10-24 19:26:00 +0700

    errrrmcat

--- anykernel.sh
+++ anykernel.sh
@@ -25,8 +25,8 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 
 ## boot shell variables
-block=/dev/block/platform/bootdevice/by-name/boot;
-is_slot_device=0;
+block=/dev/block/by-name/boot;
+is_slot_device=1;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
