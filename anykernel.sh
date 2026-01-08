3992164bca68f4d6b44349e1b403d35fd883eea0
fukiame <fukiame@proton.me>
2026-01-08 09:49:46 +0700

    tested working 2026
    Signed-off-by: fukiame <fukiame@proton.me>

--- anykernel.sh
+++ anykernel.sh
@@ -10,9 +10,7 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=selene
-device.name2=selenes
-device.name3=eos
+device.name1=judypn
 supported.versions=
 supported.patchlevels=
 '; } # end properties
@@ -26,7 +24,7 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 
 ## boot shell variables
-block=/dev/block/platform/bootdevice/by-name/boot;
+block=/dev/block/by-name/boot;
 is_slot_device=1;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
