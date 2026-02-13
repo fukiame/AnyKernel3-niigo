96aeb152d7439829443d6dae0a90cd557da56bba
fukiame <fukiame@proton.me>
2026-02-14 01:25:52 +0700

    seems agressive
    Signed-off-by: fukiame <fukiame@proton.me>

--- anykernel.sh
+++ anykernel.sh
@@ -24,7 +24,7 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 
 ## boot shell variables
-block=/dev/block/by-name/boot;
+block=/dev/block/platform/13540000.dwmmc0/by-name/BOOT;
 is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
